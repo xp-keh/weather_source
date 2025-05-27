56f064c09e6cda0e7cdffc189d9075425654ec53
ernestbenedictus <m010d4ky1623@bangkit.academy>
2025-05-27 15:37:12 +0700

    chore: changed indentation

--- produce/kafka.py
+++ produce/kafka.py
@@ -47,49 +47,49 @@ class Producer:
             return bytes(message, encoding='utf-8')
         return message
     
-def produce(self) -> None:
-    try:
-        self.logger.info(" [*] Starting real-time Kafka producer.")
-        api_key = get_env_value('OPENWEATHER_API_KEY')
+    def produce(self) -> None:
+        try:
+            self.logger.info(" [*] Starting real-time Kafka producer.")
+            api_key = get_env_value('OPENWEATHER_API_KEY')
 
-        locations = {
-            "TNTI": ("0.7718", "127.3667"),
-            "PMBI": ("-2.9024", "104.6993"),
-            "BKB": ("-1.1073", "116.9048"),
-            "SOEI": ("-9.7553", "124.2672"),
-            "MMRI": ("-8.6357", "122.2376"),
-        }
+            locations = {
+                "TNTI": ("0.7718", "127.3667"),
+                "PMBI": ("-2.9024", "104.6993"),
+                "BKB": ("-1.1073", "116.9048"),
+                "SOEI": ("-9.7553", "124.2672"),
+                "MMRI": ("-8.6357", "122.2376"),
+            }
 
-        while True:
-            for location, coords in locations.items():
-                lat, lon = coords
+            while True:
+                for location, coords in locations.items():
+                    lat, lon = coords
 
-                url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
+                    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}"
 
-                try:
-                    response = requests.get(url, timeout=5)
-                    response.raise_for_status()
-                    response_json = response.json()
+                    try:
+                        response = requests.get(url, timeout=5)
+                        response.raise_for_status()
+                        response_json = response.json()
 
-                    response_json["location"] = location
-                    response_json["raw_produce_dt"] = int(datetime.now().timestamp() * 1_000_000)
-                    response_json["lat"] = lat
-                    response_json["lon"] = lon
+                        response_json["location"] = location
+                        response_json["raw_produce_dt"] = int(datetime.now().timestamp() * 1_000_000)
+                        response_json["lat"] = lat
+                        response_json["lon"] = lon
 
-                    self._instance.send(self._kafka_topic, value=response_json)  # type: ignore
+                        self._instance.send(self._kafka_topic, value=response_json)  # type: ignore
 
-                    self.logger.info(f"Data sent to Kafka topic: {response_json}")
+                        self.logger.info(f"Data sent to Kafka topic: {response_json}")
 
-                except requests.exceptions.RequestException as e:
-                    logger.error(f"Error fetching weather data for {location}: {e}")
-                    continue
+                    except requests.exceptions.RequestException as e:
+                        logger.error(f"Error fetching weather data for {location}: {e}")
+                        continue
 
-            time.sleep(1)
+                time.sleep(1)
 
-    except Exception as e:
-        pass
-        self.logger.error(f" Error in Kafka: {e}")
-        self.logger.info(" [*] Stopping data generation.")
-        os._exit(1)
-    finally:
-        self._instance.close()  # type: ignore
+        except Exception as e:
+            pass
+            self.logger.error(f" Error in Kafka: {e}")
+            self.logger.info(" [*] Stopping data generation.")
+            os._exit(1)
+        finally:
+            self._instance.close()  # type: ignore
